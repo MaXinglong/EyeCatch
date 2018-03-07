90648bdd52655c5af4b3e8ca1469ab49c99c18b1
maxinglong <1450271751@qq.com>
2018-03-07 12:52:27 +0800

    fix get data from wrong place

--- EyeCatch/getData.py
+++ EyeCatch/getData.py
@@ -3,12 +3,12 @@ import os
 
 def get_data():
     """only get the first dir"""
-    root_dirs = os.listdir('./data')
+    root_dirs = os.listdir('../data')
 
     # x
     for root in root_dirs:
-        X = np.load('./data/' + root + '/' + root + '-x.npy')
-        Y = np.load('./data/' + root + '/' + root + '-y.npy')
+        X = np.load('../data/' + root + '/' + root + '-x.npy')
+        Y = np.load('../data/' + root + '/' + root + '-y.npy')
         break
 
     return X, Y
